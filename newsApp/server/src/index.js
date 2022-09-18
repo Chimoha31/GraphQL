5d89b8c5f61ac4e4cdd9b95e1387ac159b89a6a8
Chimoha31 <mokochii1108@gmail.com>
2022-09-18 16:00:41 -0700

    init

---
+++ newsApp/server/src/index.js
@@ -0,0 +1,60 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+let links = [
+  {
+    id: "link-0",
+    description: "Learn GraphQL at Udemy",
+    url: "www.udemy-graphql-tutorial.com",
+  },
+];
+
+// typeDefs = Schemaをセット
+// !は、空であってはならない(nullはダメ)
+const typeDefs = gql`
+  type Query {
+    info: String!
+    feed: [Link]!
+  }
+
+  type Mutation {
+    post(url: String!, description: String!): Link!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+`;
+
+// リゾルバ関数。(*Queryとresolversのinfoの文字列は合わせる事！)
+const resolvers = {
+  Query: {
+    info: () => "News App Clone",
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+      return link;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// playground = postmanのGraphQL版
+server.listen().then(({ url }) => {
+  console.log(`${url} is working 🏃🏼‍♀️`);
+});
