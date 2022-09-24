de5724e81be371bb6c74fbe3aebc2fab382e1fd2
Chimoha31 <mokochii1108@gmail.com>
2022-09-24 16:46:24 -0700

    Added

---
+++ newsApp/server/src/script.js
@@ -0,0 +1,25 @@
+// This is a client library to access to the database
+const { PrismaClient } = require("@prisma/client");
+const { makeStrictEnum } = require("@prisma/client/runtime");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  //schema.prismaのfileでLinkとしているから、以下もprisma.linkにしなければならない
+  const newLink = await prisma.link.create({
+    data: {
+      description: "Learn GraphQL at Udemy",
+      url: "www.udemy-graphql-tutorial.com",
+    },
+  });
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    prisma.$disconnect;
+  });
